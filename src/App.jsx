43c6bab115ae742810d2512bcbebd5620cb4296f
Natalle <natallemoura7@gmail.com>
2022-05-17 18:37:32 +0200

    Navigation bar layout and css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,77 @@
 import Navbar from "./components/Navbar";
 import UserDashboard from "./pages/UserDashboard";
+import "./styles/base.css";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <UserDashboard />;
+      <UserDashboard />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil expedita
+      possimus beatae hic reprehenderit et ut magnam quis magni nisi, vel
+      reiciendis dolores accusantium ipsum odio quod labore unde dignissimos!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed quisquam
+      dolor a consequatur modi laudantium porro qui voluptatem nihil itaque vel
+      maiores voluptate corrupti animi ex nemo, incidunt enim perspiciatis!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+      laudantium voluptate iure commodi saepe odit voluptatibus dolorem aliquid.
+      Illo nisi voluptatem dolorem asperiores dicta quasi tempora eligendi nam
+      facere perspiciatis. Lorem ipsum, dolor sit amet consectetur adipisicing
+      elit. Nihil expedita possimus beatae hic reprehenderit et ut magnam quis
+      magni nisi, vel reiciendis dolores accusantium ipsum odio quod labore unde
+      dignissimos! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
+      quisquam dolor a consequatur modi laudantium porro qui voluptatem nihil
+      itaque vel maiores voluptate corrupti animi ex nemo, incidunt enim
+      perspiciatis! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Aliquam laudantium voluptate iure commodi saepe odit voluptatibus dolorem
+      aliquid. Illo nisi voluptatem dolorem asperiores dicta quasi tempora
+      eligendi nam facere perspiciatis. Lorem ipsum, dolor sit amet consectetur
+      adipisicing elit. Nihil expedita possimus beatae hic reprehenderit et ut
+      magnam quis magni nisi, vel reiciendis dolores accusantium ipsum odio quod
+      labore unde dignissimos! Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Sed quisquam dolor a consequatur modi laudantium porro
+      qui voluptatem nihil itaque vel maiores voluptate corrupti animi ex nemo,
+      incidunt enim perspiciatis! Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Aliquam laudantium voluptate iure commodi saepe odit
+      voluptatibus dolorem aliquid. Illo nisi voluptatem dolorem asperiores
+      dicta quasi tempora eligendi nam facere perspiciatis. Lorem ipsum, dolor
+      sit amet consectetur adipisicing elit. Nihil expedita possimus beatae hic
+      reprehenderit et ut magnam quis magni nisi, vel reiciendis dolores
+      accusantium ipsum odio quod labore unde dignissimos! Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Sed quisquam dolor a consequatur
+      modi laudantium porro qui voluptatem nihil itaque vel maiores voluptate
+      corrupti animi ex nemo, incidunt enim perspiciatis! Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Aliquam laudantium voluptate iure
+      commodi saepe odit voluptatibus dolorem aliquid. Illo nisi voluptatem
+      dolorem asperiores dicta quasi tempora eligendi nam facere perspiciatis.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil expedita
+      possimus beatae hic reprehenderit et ut magnam quis magni nisi, vel
+      reiciendis dolores accusantium ipsum odio quod labore unde dignissimos!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed quisquam
+      dolor a consequatur modi laudantium porro qui voluptatem nihil itaque vel
+      maiores voluptate corrupti animi ex nemo, incidunt enim perspiciatis!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+      laudantium voluptate iure commodi saepe odit voluptatibus dolorem aliquid.
+      Illo nisi voluptatem dolorem asperiores dicta quasi tempora eligendi nam
+      facere perspiciatis. Lorem ipsum, dolor sit amet consectetur adipisicing
+      elit. Nihil expedita possimus beatae hic reprehenderit et ut magnam quis
+      magni nisi, vel reiciendis dolores accusantium ipsum odio quod labore unde
+      dignissimos! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
+      quisquam dolor a consequatur modi laudantium porro qui voluptatem nihil
+      itaque vel maiores voluptate corrupti animi ex nemo, incidunt enim
+      perspiciatis! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Aliquam laudantium voluptate iure commodi saepe odit voluptatibus dolorem
+      aliquid. Illo nisi voluptatem dolorem asperiores dicta quasi tempora
+      eligendi nam facere perspiciatis. Lorem ipsum, dolor sit amet consectetur
+      adipisicing elit. Nihil expedita possimus beatae hic reprehenderit et ut
+      magnam quis magni nisi, vel reiciendis dolores accusantium ipsum odio quod
+      labore unde dignissimos! Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Sed quisquam dolor a consequatur modi laudantium porro
+      qui voluptatem nihil itaque vel maiores voluptate corrupti animi ex nemo,
+      incidunt enim perspiciatis! Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Aliquam laudantium voluptate iure commodi saepe odit
+      voluptatibus dolorem aliquid. Illo nisi voluptatem dolorem asperiores
+      dicta quasi tempora eligendi nam facere perspiciatis.
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,54 @@
-import logo from "../assets/logo.svg";
+import logo from "../assets/icons-netflix/logo-navbar.png";
+import mag from "../assets/icons/search-icon.png";
+import bell from "../assets/icons/notification-icon.png";
+import userArrow from "../assets/icons/user-arrow.png";
+import userImg from "../assets/images/user-profile.png";
+import "../styles/components/navbar.css";
+
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const changeBackgroundColor = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 80) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackgroundColor();
+    // adding the event to listen to the scroll and call the change
+    window.addEventListener("scroll", changeBackgroundColor);
+  });
+
   return (
-    <nav>
-      <div>
-        <div>
-          <img src={logo} alt="Nättflix website" className="logo" />
-        </div>
-        <div className="list-container">
-          <ul className="list">
-            <li>Home</li>
-            <li>Series</li>
-            <li>Films</li>
-            <li>New & Popular</li>
-            <li>My list</li>
-          </ul>
-        </div>
+    <nav className={isScrolled ? "navigation-bar scrolled" : "navigation-bar"}>
+      <div className="logo-navigation-container">
+        <img src={logo} alt="Nättflix website" className="netflix-logo" />
+        <ul className="list-navigation">
+          <li className="navigation-focus">Home</li>
+          <li>Series</li>
+          <li>Films</li>
+          <li>New & Popular</li>
+          <li>My list</li>
+        </ul>
       </div>
       <div>
-        <ul>
-          <li>Search</li>
-          <li>Notification</li>
-          <li>User</li>
+        <ul className="secondary-navigation">
+          <li>
+            <img src={mag} alt="search" className="secondary-icons" />
+          </li>
+          <li>
+            <img src={bell} alt="notifications" className="secondary-icons" />
+          </li>
+          <li className="user-options">
+            <img src={userImg} alt="" className="user-avatar" />{" "}
+            <img src={userArrow} alt="" className="user-arrow-options" />{" "}
+          </li>
         </ul>
       </div>
     </nav>
